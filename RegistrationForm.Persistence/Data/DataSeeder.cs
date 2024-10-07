45251a7ab88f9aa814f81f42dc2e5d83d3e9c998
Yousef Osman <yousef.osman.31@gmail.com>
2024-10-07 18:34:52 +0300

    update database migrations and seed data

---
+++ RegistrationForm.Persistence/Data/DataSeeder.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RegistrationForm.Domain.Entities;
+
+namespace RegistrationForm.Persistence.Data;
+public static class DataSeeder
+{
+    public static async Task SeedAsync(IServiceProvider serviceProvider)
+    {
+        using var scope = serviceProvider.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        await SeedGovernatesAndCities(context);
+
+        if (context.ChangeTracker.HasChanges())
+            await context.SaveChangesAsync();
+    }
+
+    private static async Task SeedGovernatesAndCities(AppDbContext context)
+    {
+        if (await context.Governates.AnyAsync())
+            return;
+
+        var types = new List<Governate>
+        {
+            new Governate 
+            {
+                Name = "Cairo",
+                Cities = new List<City>
+                {
+                    new City { Name = "5th Settlement" },
+                    new City { Name = "Ain Shams" },
+                    new City { Name = "Maadi" },
+                }
+            },
+            new Governate 
+            { 
+                Name = "Alexandria",
+                Cities = new List<City>
+                {
+                    new City { Name = "El Montaza"},
+                    new City { Name = "Moharam Bek" },
+                    new City { Name = "Sidi Gaber" },
+                }
+            },
+            new Governate 
+            { 
+                Name = "Giza",
+                Cities = new List<City>
+                {
+                    new City { Name = "Dokki" },
+                    new City { Name = "Pyramids" },
+                    new City { Name = "Agouza" },
+            
+                }
+            },
+            new Governate 
+            { 
+                Name = "Aswan",
+                Cities = new List<City>
+                {
+                    new City { Name = "Abu Simbel" },
+                    new City { Name = "Edfu" },
+                    new City { Name = "Kom Ombo"},
+                }
+            },
+            new Governate 
+            { 
+                Name = "Luxor",
+                Cities = new List<City>
+                {
+                    new City { Name = "Qurna" },
+                    new City { Name = "Armant" },
+                    new City { Name = "Thebes " },
+                }
+            },
+        };
+
+        context.Governates.AddRange(types);
+    }
+}
---
+++ RegistrationForm.Persistence/Data/Migrations/20241006190038_AddRelationBetweenCityAndGovernateAndRemoveGovernateFromAddress.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistrationForm.Persistence.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRelationBetweenCityAndGovernateAndRemoveGovernateFromAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_Governates_GovernateId",
+                table: "Addresses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Addresses_GovernateId",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "GovernateId",
+                table: "Addresses");
+
+            migrationBuilder.AddColumn<long>(
+                name: "GovernateId",
+                table: "Cities",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_GovernateId",
+                table: "Cities",
+                column: "GovernateId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cities_Governates_GovernateId",
+                table: "Cities",
+                column: "GovernateId",
+                principalTable: "Governates",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cities_Governates_GovernateId",
+                table: "Cities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cities_GovernateId",
+                table: "Cities");
+
+            migrationBuilder.DropColumn(
+                name: "GovernateId",
+                table: "Cities");
+
+            migrationBuilder.AddColumn<long>(
+                name: "GovernateId",
+                table: "Addresses",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_GovernateId",
+                table: "Addresses",
+                column: "GovernateId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_Governates_GovernateId",
+                table: "Addresses",
+                column: "GovernateId",
+                principalTable: "Governates",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
